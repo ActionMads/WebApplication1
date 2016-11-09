66dbf1b91cb586de76685c88bef34ef771b8e717
ActionMads <madsvmunk@gmail.com>
2016-11-09 15:16:22 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            using(var _context = ApplicationDbContext.Create())
+            {
+                var customer = new Customer();
+                customer.Company = "It-minds";
+                _context.Customers.Add(customer);
+
+                var employee = new Employee();
+                _context.Employees.Add(employee);
+
+                _context.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string Company { get; set; }
+
+        public virtual int EmployeeId { get; set; }
+
+
+    }
+}
